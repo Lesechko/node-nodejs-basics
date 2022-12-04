5a26065e1165674c6907c4396857c2dbbd3dc876
Oleksii Lesechko <alekseylesechko@gmail.com>
2022-12-04 21:21:38 +0200

    add cli tasks

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,10 @@
 const parseArgs = () => {
-    // Write your code here 
+  const argv = process.argv.slice(2);
+
+  argv.forEach(
+    (argv, idx, argvs) =>
+      idx % 2 === 0 && console.log(`${argv} is ${argvs[idx + 1]}`)
+  );
 };
 
 parseArgs();
--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,12 @@
 const parseEnv = () => {
-    // Write your code here 
+  const targetKey = "RSS_";
+  const env = Object.keys(process.env).filter((key) =>
+    key.startsWith(targetKey)
+  );
+
+  env.forEach((key) => {
+    console.log(`${key}=${process.env[key]}`);
+  });
 };
 
 parseEnv();
