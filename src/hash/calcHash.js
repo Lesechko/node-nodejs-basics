362f55c67c9a25104cd9b5e1389a60c875f67014
Oleksii Lesechko <alekseylesechko@gmail.com>
2022-12-04 21:34:28 +0200

    add hash task

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,29 @@
+import * as fs from "fs";
+import * as path from "path";
+import { fileURLToPath } from "url";
+import { createHash } from "node:crypto";
+
+const join = (...args) => {
+  const __filename = fileURLToPath(import.meta.url);
+  const __dirname = path.dirname(__filename);
+
+  return path.join(__dirname, ...args);
+};
+
 const calculateHash = async () => {
-    // Write your code here 
+  const filePath = join("files", "fileToCalculateHashFor.txt");
+  const errorMessage = "FS operation failed";
+
+  fs.readFile(filePath, {}, (err, data) => {
+    if (err) {
+      throw new Error(errorMessage);
+    }
+
+    if (data) {
+      const hex = createHash("sha3-256").update(data).digest("hex");
+      console.log(hex);
+    }
+  });
 };
 
 await calculateHash();
