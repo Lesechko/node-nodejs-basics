b15278b6716993fee5a48e29d51820363ab9f0b6
Oleksii Lesechko <alekseylesechko@gmail.com>
2022-12-04 22:01:29 +0200

    add zip tasks

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,5 +1,25 @@
+import { createReadStream, createWriteStream } from "fs";
+import { createGzip } from "zlib";
+import { fileURLToPath } from "url";
+import * as path from "path";
+
+const join = (...args) => {
+  const __filename = fileURLToPath(import.meta.url);
+  const __dirname = path.dirname(__filename);
+
+  return path.join(__dirname, ...args);
+};
+
 const compress = async () => {
-    // Write your code here 
+  const src = join("files", "fileToCompress.txt");
+  const dest = join("files", "archive.gz");
+
+  createReadStream(src)
+    .pipe(createGzip())
+    .pipe(createWriteStream(dest))
+    .on("finish", () => {
+      console.log(`Compression process done: ${src}`);
+    });
 };
 
 await compress();
--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,5 +1,25 @@
+import { createReadStream, createWriteStream } from "fs";
+import { createUnzip } from "zlib";
+import { fileURLToPath } from "url";
+import * as path from "path";
+
+const join = (...args) => {
+  const __filename = fileURLToPath(import.meta.url);
+  const __dirname = path.dirname(__filename);
+
+  return path.join(__dirname, ...args);
+};
+
 const decompress = async () => {
-    // Write your code here 
+  const dest = join("files", "fileToCompress2.txt");
+  const src = join("files", "archive.gz");
+
+  createReadStream(src)
+    .pipe(createUnzip())
+    .pipe(createWriteStream(dest))
+    .on("finish", () => {
+      console.log(`Decompression process done: ${src}`);
+    });
 };
 
 await decompress();
